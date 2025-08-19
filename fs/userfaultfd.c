764c9d62adb1c33b14c893bfe93cfbf3b2aea2b2
mayuresh2543 <mayureshnanal846@gmail.com>
2025-08-19 12:07:43 +0530

    fs: userfaultfd: remove vm_write_begin(vma)

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -1652,7 +1652,6 @@ static int userfaultfd_unregister(struct userfaultfd_ctx *ctx,
 		 * the next vma was merged into the current one and
 		 * the current one has not been updated yet.
 		 */
-		vm_write_begin(vma);
 		vma->vm_flags = vma_pad_fixup_flags(vma, new_flags);
 		vma->vm_userfaultfd_ctx = NULL_VM_UFFD_CTX;
 
